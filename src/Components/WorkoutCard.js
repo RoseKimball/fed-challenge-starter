62378b1bdc7b2c2eb8f52430c0e71df82d5b3274
RoseKimball <mrkimbal@usc.edu>
2021-03-21 19:39:08 -0700

    fixed image importing

--- src/Components/WorkoutCard.js
+++ src/Components/WorkoutCard.js
@@ -4,21 +4,21 @@ import '../Styles.css';
 import Playlist from './Playlist';
 
 const WorkoutCard = ({workout}) => {
-    
     return (
         
         <>
             {workout.map((w) => {
+                // console.log(w.image);
                 return (
                     <div key={w.title} className='WorkoutCard'>
                         <div className='Image'>
-                            <img src={'/assets/' + w.image} alt='workoutImg'></img>
+                            <img src={process.env.PUBLIC_URL +'/assets/' + w.image} alt='workoutImg'></img>
                             {w.workouts > 1 ? (<Playlist number={w.workouts}/>) : null}
                         </div>
                             <div className='Title'>
                                 <h3 className='TitleText'>{w.title}</h3>
                             <div>
-                                <img src={'/assets/' + w.instructorImage} alt='trainer'></img>
+                                <img src={process.env.PUBLIC_URL +'/assets/' + w.instructorImage} alt='trainer'></img>
                             </div>
                         </div>
                         {w.length && w.distance ? (
